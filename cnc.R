b16c1346c5c596d5e31f6ce501c8f9fb9bee2ec6
aassumpcao <andre.assumpcao@gmail.com>
2018-10-23 11:58:44 -0400

    working on cnc data

--- cnc.R
+++ cnc.R
@@ -1,71 +1,181 @@
-################################################################################
-# script to download data from the Cadastro Nacional de Condenações Cíveis
-# por Ato de Improbidade Administrativa e Inelegibilidade
+Skip to content
 
-# author:
-# Andre Assumpcao
-# andre.assumpcao@gmail.com
+Search or jump to…
 
-# clear environment
-rm(list = objects())
+Pull requests
+Issues
+Marketplace
+Explore
+ @aassumpcao Sign out
+7
+5 1 abjur/cnc
+ Code  Issues 1  Pull requests 0  Projects 0  Wiki  Insights
+cnc/R/cnc.R
+4d5daaa  on Dec 19, 2016
+@jtrecenti jtrecenti correct param name
 
-# import statements
-library(magrittr)
-library(tidyverse)
-library(cnc)
-library(rvest)
-library(xml2)
+Executable File  152 lines (145 sloc)  5.8 KB
+#' Baixa uma página web qualquer usando PhantomJS
+#'
+#' Acessa uma URL, roda javascripts e baixa página usando PhantomJS.
+#' Para funcionar, é necessário instalar o PhamtomJS
+#' (\url{http://phantomjs.org/download.html}).
+#'
+#' @param url endereço web que se deseja baixar.
+#' @param arq caminho do arquivo para baixar.
+#'
+#' @return retorna \code{TRUE} se der tudo certo e ele conseguir criar e deletar todos os arquivos temporários.
+#'
+#' @export
+GET_pjs <- function(url, arq = 'arq.html') {
+  tmp <- tempfile('scrape', fileext = '.js')
+  writeLines(sprintf("var page = require('webpage').create();
+    page.open('%s', function () {
+        console.log(page.content); //page source
+        phantom.exit();
+    });", url), con = tmp)
+  system(sprintf("phantomjs %s > %s", tmp, arq))
+  file.remove(tmp)
+}
 
-cnc_pags(path = 'data-raw/pags', pags = 1)
-d_pags <- 'data-raw/pags' %>%
-  dir(full.names = TRUE) %>%
-  parse_pags()
-
-# baixa pessoas e processos
-cnc_pessoas(d_pags, path = 'data-raw/pessoas')
-cnc_processos(d_pags, path = 'data-raw/processos')
-
-# parse pessoas e processos
-d_pessoas <- 'data-raw/pessoas' %>%
-  dir(full.names = TRUE) # %>%
-  parse_pessoas(d_pessoas)
-
-d_processos <- 'data-raw/processos' %>%
-  dir(full.names = TRUE) %>%
-  parse_processos()
-
-# baixa infos das pessoas
-cnc_pessoas_infos(d_pessoas, path = 'data-raw/pessoas_infos')
-# parse infos das pessoas
-d_pessoas_infos <- 'data-raw/pessoas_infos' %>%
-  dir(full.names = TRUE) %>%
-  parse_pessoas_infos(arqs)
-
-
-arq <- d_pessoas[1]
-
-
-
-arq %>%
-  xml2::read_html() %>%
-  rvest::html_nodes('table') %>%
-  dplyr::first() %>% {
-    h <- .
-    tb <- h %>%
-      rvest::html_table(header = TRUE) %>%
-      setNames(c('nm_pessoa', 'num_processo')) %>%
-      dplyr::mutate(id = 1:n()) %>%
-      tidyr::gather(key, value, -id) %>%
-      dplyr::mutate(value = stringr::str_replace_all(value, stringr::fixed('\\'), '@'),
-                    value = stringr::str_replace_all(value, "\n|\t|@.", ''),
-                    value = stringr::str_trim(value))
-    l <- h %>%
-      rvest::html_nodes('a') %>%
-      rvest::html_attr('href') %>%
-      stringr::str_replace_all("'|\\\\", '') %>% {
-        c(.[stringr::str_detect(., '_condenacao')],
-          .[stringr::str_detect(., '_processo')])
-      }
-    tb %>%
-      dplyr::mutate(link = l)
+cnc_pag <- function(p, path, parse = TRUE) {
+  x <- 'http://www.cnj.jus.br/improbidade_adm/consultar_requerido.php?validar=form&rs=pesquisarRequeridoGetTabela&rst=&rsrnd=0&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=I&rsargs[]=0&rsargs[]=POSICAO_INICIAL_PAGINACAO_PHP%s&rsargs[]=QUANTIDADE_REGISTROS_PAGINACAO15'
+  u <- sprintf(x, as.character((p - 1) * 15))
+  arq <- sprintf('%s/pag_%05d.html', path, p)
+  if (!file.exists(arq)) {
+    r <- httr::GET(u, httr::write_disk(arq, overwrite = TRUE))
+    tibble::data_frame(result = 'OK')
+  } else {
+    tibble::data_frame(result = 'arquivo existe')
   }
+}
+
+#' Baixa páginas do CNC
+#'
+#' Acessa todas as páginas do CNC indicadas e salva em arquivos HTML. Acessa páginas do tipo
+#' \url{http://www.cnj.jus.br/improbidade_adm/consultar_requerido.php?validar=form&rs=pesquisarRequeridoGetTabela&rst=&rsrnd=0&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=&rsargs[]=I&rsargs[]=0&rsargs[]=POSICAO_INICIAL_PAGINACAO_PHP1&rsargs[]=QUANTIDADE_REGISTROS_PAGINACAO15}
+#'
+#' @param path caminho da pasta onde os arquivos HTML serão salvos. Se a pasta não existir, será criada.
+#' @param pags páginas a serem baixadas. Default 1:2399 (não atualiza com muita frequênica)
+#'
+#' @return \code{data.frame} indicando "OK" se baixou corretamente e "arquivo existe" se o arquivo já existe.
+#'
+#' @export
+cnc_pags <- function(path = 'data-raw/pags', pags = 1:2399) {
+  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+  tibble::data_frame(pag = pags) %>%
+    dplyr::group_by(pag) %>%
+    dplyr::do(cnc_pag(.$pag, path)) %>%
+    dplyr::ungroup()
+}
+
+#' Baixa pessoas
+#'
+#' Baixa HTMLs de pesquisas do tipo
+#' \url{http://www.cnj.jus.br/improbidade_adm/visualizar_condenacao.php?seq_condenacao=1101}
+#'
+#' @param dados retornados pela função \code{\link{parse_pags}}.
+#' @inheritParams cnc_pags
+#'
+#' @return \code{data.frame} indicando "OK" se baixou corretamente e "arquivo existe" se o arquivo já existe.
+#'
+#' @export
+cnc_pessoas <- function(dados, path = 'data-raw/pessoas') {
+  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+  dados %>%
+    dplyr::filter(key == 'nm_pessoa') %>%
+    dplyr::distinct(link) %>%
+    dplyr::mutate(num_link = gsub('[^0-9]', '', link)) %>%
+    dplyr::group_by(link, num_link) %>%
+    dplyr::do({
+      l <- paste0('http://www.cnj.jus.br/improbidade_adm/', .$link)
+      a <- sprintf('%s/%s.html', path, .$num_link)
+      if (!file.exists(a)) {
+        # httr::GET(l, httr::write_disk(a, overwrite = TRUE))
+        GET_pjs(l, a)
+        tibble::data_frame(result = 'OK')
+      } else {
+        tibble::data_frame(result = 'ja existe')
+      }
+    }) %>%
+    dplyr::ungroup()
+}
+
+#' Baixa processos
+#'
+#' Baixa HTMLs de pesquisas do tipo
+#' \url{http://www.cnj.jus.br/improbidade_adm/visualizar_processo.php?seq_processo=9421}
+#'
+#' @param dados retornados pela função \code{\link{parse_pags}}.
+#' @inheritParams cnc_pags
+#'
+#' @return \code{data.frame} indicando "OK" se baixou corretamente e "arquivo existe" se o arquivo já existe.
+#'
+#' @export
+cnc_processos <- function(dados, path = 'data-raw/processos') {
+  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+  dados %>%
+    dplyr::filter(key == 'num_processo') %>%
+    dplyr::distinct(link) %>%
+    dplyr::mutate(num_link = gsub('[^0-9]', '', link)) %>%
+    dplyr::group_by(link, num_link) %>%
+    dplyr::do({
+      l <- paste0('http://www.cnj.jus.br/improbidade_adm/', .$link)
+      a <- sprintf('%s/%s.html', path, .$num_link)
+      if (!file.exists(a)) {
+        GET_pjs(l, a)
+        # httr::GET(l, httr::write_disk(a, overwrite = TRUE))
+        tibble::data_frame(result = 'OK')
+      } else {
+        tibble::data_frame(result = 'ja existe')
+      }
+    }) %>%
+    dplyr::ungroup()
+}
+
+#' Baixa infos de pessoas
+#'
+#' Baixa HTMLs de pesquisas do tipo
+#' \url{http://www.cnj.jus.br/improbidade_adm/visualizar_condenacao.php?seq_condenacao=1&rs=getDadosParte&rst=&rsrnd=0&rsargs[]=1}
+#'
+#' @param d_pessoas retornados pela função \code{\link{parse_pessoas}}.
+#' @inheritParams cnc_pags
+#'
+#' @return \code{data.frame} indicando "OK" se baixou corretamente e "arquivo existe" se o arquivo já existe.
+#'
+#' @export
+cnc_pessoas_infos <- function(d_pessoas, path = 'data-raw/pessoas_infos') {
+  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+  id_pessoa <- d_pessoas %>%
+    dplyr::filter(stringr::str_detect(value, 'recuperarDados')) %>%
+    with(gsub('[^0-9]', '', value))
+  l <- 'http://www.cnj.jus.br/improbidade_adm/visualizar_condenacao.php?seq_condenacao=1&rs=getDadosParte&rst=&rsrnd=0&rsargs[]=%s'
+  l <- sprintf(l, id_pessoa)
+  tibble::data_frame(id = id_pessoa, link = l) %>%
+    dplyr::group_by(id, link) %>%
+    dplyr::do({
+      a <- sprintf('%s/%s.html', path, .$id)
+      if (!file.exists(a)) {
+        httr::GET('http://www.cnj.jus.br/improbidade_adm/visualizar_condenacao.php?seq_condenacao=1')
+        httr::GET(.$link, httr::write_disk(a, overwrite = TRUE))
+        tibble::data_frame(result = 'OK')
+      } else {
+        tibble::data_frame(result = 'ja existe')
+      }
+    }) %>%
+    dplyr::ungroup()
+}
+
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Press h to open a hovercard with more details.
