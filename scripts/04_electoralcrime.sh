4cbeb12f1af6c4eca3f00d1e11a05f2458242f71
andre.assumpcao@gmail.com <andre.assumpcao@gmail.com>
2019-09-22 22:22:19 -0400

    finished classifying 2012 sentences

--- scripts/04_electoralcrime.sh
+++ scripts/04_electoralcrime.sh
@@ -3,10 +3,10 @@
 #SBATCH -p bigmem
 #SBATCH --qos bigmem_access
 #SBATCH -N 1
-#SBATCH --mem=1000g
+#SBATCH --mem=2000g
 #SBATCH -n 16
-#SBATCH -t 7-
+#SBATCH -t 11-
 #SBATCH --mail-user=andre.assumpcao@gmail.com
 #SBATCH --mail-type=ALL
 
-python3.6 scripts/10_tse_sentence_classification.py
+python3.6 scripts/11_tse_sentence_classification.py --chi2_select=20000
